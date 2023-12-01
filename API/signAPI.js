8e4f875582a7430c02cc0eb31c903896e5e857ff
byj9402 <byj9402@naver.com>
2023-12-01 17:03:47 +0900

    [Chore]: change file name

--- API/signAPI.js
+++ API/signAPI.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { signUp } = require("../Models/signUpModel");
-const { signIn } = require("../Models/signInModel");
+const { signIn } = require("../Models/SignInModel");
 
 router.post("/signUp", async (req, res) => {
   try {
